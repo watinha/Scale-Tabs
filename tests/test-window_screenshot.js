8d3a34043fbdccbbfed1aee01541b1c88b68e82e
Willian Massami Watanabe <watinha@gmail.com>
2011-05-29 21:54:09 -0300

    Take out the underterminism of unit-tests for window-screenshot

--- tests/test-window_screenshot.js
+++ tests/test-window_screenshot.js
@@ -4,27 +4,27 @@ var data = require("self").data;
 var timer = require("timer");
 
 exports.tab_opening = function(test){
-  tabs.open("http://www.google.com");
-  tabs.open("http://www.yahoo.com");
-  tabs.open("http://www.twitter.com");
+  tabs.open("about:blank");
+  tabs.open("about:blank");
+  tabs.open("about:addons");
 
   var count_tabs_loaded = 0;
   tabs.on("ready", function(tab){
-    count_tabs_loaded++; 
+    count_tabs_loaded++;
     window_screenshot.load_tab_data(tab);
     test.assertEqual(tab.screenshot.substring(0,4), "data", "Testing if the TAB Jetpack object was enhanced with the screenshot attribute for tab" + tab.index);
     if(count_tabs_loaded == 3)
       test.done();
   });
-  
+
   test.waitUntilDone(30000);
 };
 
 exports.tab_changing = function(test){
-  tabs.open("http://www.facebook.com");
+  tabs.open("about:blank");
   tabs.on("ready", function(tab){
-    window_screenshot.change_tab("Google");
-    timer.setTimeout(function(){test.assertEqual(tabs.activeTab.title, "Google", "Testing if the tab change functionality is working.");}, 200);
+    window_screenshot.change_tab("Complementos");
+    timer.setTimeout(function(){test.assertEqual(tabs.activeTab.title, "Complementos", "Testing if the tab change functionality is working.");}, 200);
     timer.setTimeout(function(){test.assertEqual(tabs.length, 4, "Testing if the last tab was closed.");}, 200);
     test.done();
   });
